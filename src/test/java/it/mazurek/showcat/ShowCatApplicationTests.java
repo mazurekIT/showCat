e6fef939cf51316285443214f6fcf8dd21dc18db
Mazurek Mateusz <mazzi198913@wp.pl>
2023-03-07 20:33:16 +0100

    Create ShowCatApplicationTests.java
    start commit

---
+++ src/test/java/it/mazurek/showcat/ShowCatApplicationTests.java
@@ -0,0 +1,13 @@
+package it.mazurek.showcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowCatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
